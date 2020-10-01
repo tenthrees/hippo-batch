7fee43fb150ad38ce44a75d14bc9bf2c0b9ba9d1
Fasina Oreoluwa <dannyoma75@gmail.com>
2020-10-01 08:05:03 +0100

    +*

--- models/hippo.js
+++ models/hippo.js
@@ -2,6 +2,10 @@ var mongoose=require("mongoose")
 var Schema=mongoose.Schema
 
 var hippoSchema =new Schema({
+    lastMineUp : {type : Object},
+    lastMineDown : {type : Object},
+    mineAmount : {type : String},
+    mineMetrics : {type : Boolean},
     bank : {type:String},
     name:{type:String},
     bvn : {type:String},
